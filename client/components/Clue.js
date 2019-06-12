21a7ea8d12eebd3095495275c3fc8eef58aac2a6
Sarah <sarahrva@gmail.com>
2019-06-12 09:53:37 -0500

    Fix console.errors

--- client/components/Clue.js
+++ client/components/Clue.js
@@ -5,13 +5,14 @@ const Clue = props => {
   // show $ value of clue OR
   // the Clue question itself OR
   // empty screen if it was already answered
+  return 'hi';
 };
 
 Clue.propTypes = {
   selected: PropTypes.bool,
   selectQuestion: PropTypes.func,
   answered: PropTypes.bool,
-  clueObject: PropTypes.object,
-}
+  clueObject: PropTypes.object
+};
 
 export default Clue;
